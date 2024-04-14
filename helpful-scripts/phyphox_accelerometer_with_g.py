1d8367819d43e3d9a13a3a98cd5a6dbc123d23be
manavthakkar <manavt2000@gmail.com>
2024-04-14 16:32:59 +0200

    Add error handling

--- helpful-scripts/phyphox_accelerometer_with_g.py
+++ helpful-scripts/phyphox_accelerometer_with_g.py
@@ -9,17 +9,22 @@ import requests as r
 import json
 import time
 
-url = 'http://192.168.1.103:8080/get?'
+url = 'http://192.168.0.101:8080/get?'
 
 what_to_get = ['accX', 'accY', 'accZ', 'acc']
 
 def phyphox_data():
-    response = r.get(url + '&'.join(what_to_get)).text
-    data = json.loads(response)
-    for item in what_to_get:
-        acc_data = data['buffer'][item]['buffer'][0]
-        print(f'{acc_data:10.7}', end='\t')
-    print()
+    try:
+        response = r.get(url + '&'.join(what_to_get))
+        response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
+        data = response.json()
+        for item in what_to_get:
+            acc_data = data['buffer'][item]['buffer'][0]
+            print(f'{acc_data:10.7}', end='\t')
+        print()
+    except r.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        # You can add additional error handling or logging here if needed
 
 while True:
     phyphox_data()
